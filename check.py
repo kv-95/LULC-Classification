6d6b159681d389742c1130e9a99c00383167e4d4
kv-95 <49032825+kv-95@users.noreply.github.com>
2021-07-11 13:29:05 +0530

    Add files via upload

---
+++ check.py
@@ -0,0 +1,23 @@
+import dataset
+import numpy as np
+import tensorflow as tf
+import keras
+
+#Load Model
+model1 = tf.keras.models.load_model('Model/')
+
+#Checking for random image
+test_path = 'D:\MSc GIS\Sem 2\MIni project\Code\Land_Classify_Tensorflow\Test\ConfusetheModel02.jpg'
+img = keras.preprocessing.image.load_img(
+    test_path, target_size=(dataset.img_height, dataset.img_width)
+)
+img_array = keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+predictions = model1.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(dataset.train_ds.class_names[np.argmax(score)], 100 * np.max(score))
+    )
