b3388cfa290ccf269e9222e1cfcdc2fa21cfcf51
kv-95 <49032825+kv-95@users.noreply.github.com>
2021-07-28 15:08:09 +0530

    Update check.py

--- check.py
+++ check.py
@@ -3,11 +3,16 @@ import numpy as np
 import tensorflow as tf
 import keras
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+tf.config.set_visible_devices(gpus[0], 'GPU')
+
 #Load Model
 model1 = tf.keras.models.load_model('Model/')
 
 #Checking for random image
-test_path = 'test_image.jpg'
+test_path = 'image.jpg'
+
 img = keras.preprocessing.image.load_img(
     test_path, target_size=(dataset.img_height, dataset.img_width)
 )
