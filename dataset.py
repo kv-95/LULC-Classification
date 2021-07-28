5994a74e29a0e849f3ae4339627dbc498fa15628
kv-95 <49032825+kv-95@users.noreply.github.com>
2021-07-28 15:09:35 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 import pathlib
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+tf.config.set_visible_devices(gpus[0], 'GPU')
+
 data_dir = 'Data'
 data_dir = pathlib.Path(data_dir)
 
