5cad3acabe78310c7d3fd982c573bb43b52ffc92
kv-95 <49032825+kv-95@users.noreply.github.com>
2021-07-28 15:10:04 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,22 +1,32 @@
+from tensorflow.python.keras.layers.core import Activation, Dense
 import standard
 import dataset
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras import layers
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+tf.config.set_visible_devices(gpus[0], 'GPU')
+
 num_classes = len(dataset.train_ds.class_names)
 
 model = Sequential([
   standard.data_augmentation,
   layers.experimental.preprocessing.Rescaling(1./255),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
+
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Dropout(0.2),
+
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Dropout(0.1),
+
+  layers.Conv2D(128, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
   layers.Flatten(),
+
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
 ])
