81bad217f7f3a4fb1bfa0680974ec53d0b677b3b
kv-95 <49032825+kv-95@users.noreply.github.com>
2021-07-28 15:10:59 +0530

    Update classify.py

--- classify.py
+++ classify.py
@@ -1,46 +1,29 @@
-from tensorflow.python.keras.layers.core import Activation, Dense
-import standard
-import dataset
-import tensorflow as tf
-from tensorflow.keras import Sequential
-from tensorflow.keras import layers
+import model
+import matplotlib.pyplot as plt
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(gpus[0], True)
-tf.config.set_visible_devices(gpus[0], 'GPU')
+#Visualize Training results
+acc = model.history.history['accuracy']
+val_acc = model.history.history['val_accuracy']
 
-num_classes = len(dataset.train_ds.class_names)
+loss = model.history.history['loss']
+val_loss = model.history.history['val_loss']
 
-model = Sequential([
-  standard.data_augmentation,
-  layers.experimental.preprocessing.Rescaling(1./255),
+epochs_range = range(model.epochs)
 
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Dropout(0.2),
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
 
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Dropout(0.1),
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
 
-  layers.Conv2D(128, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
+#Save model
+model.model.save("Model/")
 
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
-])
-
-model.compile(optimizer='adam',
-              loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-
-#TRAINING 
-
-epochs = 15
-history = model.fit(
-  standard.train_ds,
-  validation_data = standard.val_ds,
-  epochs=epochs
-)
